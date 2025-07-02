3929d1d8fa7edb67782601e4dbd0b717d83f6d38
Xavier <xavart2011@gmail.com>
2025-07-02 22:55:47 +0200

    add secret

--- app.py
+++ app.py
@@ -7,22 +7,27 @@ from bokeh.palettes import Category10
 from firebase_admin import initialize_app, db
 
 
-pn.extension(design='material')
+pn.extension(design="material")
 if pn.state.curdoc is not None:
     pn.state.curdoc.theme = "caliber"
 tools = "pan,box_zoom,wheel_zoom,reset"
 rollover = 10000
 local_tz = ZoneInfo("Europe/Paris")
 
+
 def initialise_db():
-    initialize_app({
-        'databaseURL': 'https://dht22records-default-rtdb.europe-west1.firebasedatabase.app'
-    })
+    initialize_app(
+        "/etc/secrets/dht22records-7d6a2f605770.json",
+        {
+            "databaseURL": "https://dht22records-default-rtdb.europe-west1.firebasedatabase.app"
+        },
+    )
+
 
 def process_record_timestamp(record):
     ts_int = int(record["timestamp"])
     utc_ts = datetime.fromtimestamp(ts_int, tz=timezone.utc)
-    local_datetime =utc_ts.astimezone(local_tz)
+    local_datetime = utc_ts.astimezone(local_tz)
     record.update(
         time_str=local_datetime.strftime("%H:%M:%S [%Y-%m-%d]"),
         local_datetime=local_datetime,
@@ -44,7 +49,7 @@ setup()
 
 
 options = list(db.reference("dht_readings").get(shallow=True).keys())
-palette = Category10[10] 
+palette = Category10[10]
 colors = {opts: palette[idx] for idx, opts in enumerate(options)}
 
 cds = {}
@@ -63,31 +68,41 @@ for sensor in options:
 
 def update():
     for sensor in options:
-        new_records = list(filter(lambda r: r["local_datetime"] not in cds[sensor].data["local_datetime"], 
-            [process_record_timestamp(r) for r in db.reference(f"dht_readings/{sensor}")
-            .order_by_key()
-            .limit_to_last(3)
-            .get()
-            .values()
-            ]
-        ))
+        new_records = list(
+            filter(
+                lambda r: r["local_datetime"] not in cds[sensor].data["local_datetime"],
+                [
+                    process_record_timestamp(r)
+                    for r in db.reference(f"dht_readings/{sensor}")
+                    .order_by_key()
+                    .limit_to_last(3)
+                    .get()
+                    .values()
+                ],
+            )
+        )
         if new_records:
             cds[sensor].stream(records_to_dict(new_records), rollover=rollover)
-         
+
+
 update_cb = pn.state.add_periodic_callback(update, 10000)
+
 
 def on_session_destroyed(session_context):
     print("Session closed, stopping callback")
     update_cb.stop()
+
 
 pn.state.on_session_destroyed(on_session_destroyed)
 
 
 sensor_selection = pn.widgets.MultiSelect(options=options, name="Sensor")
 
+
 @pn.depends(sensors=sensor_selection.param.value)
 def bokeh_plot(sensors):
-    if not sensors: return
+    if not sensors:
+        return
     plot_temperature = figure(
         sizing_mode="stretch_both",
         x_axis_type="datetime",
@@ -106,11 +121,17 @@ def bokeh_plot(sensors):
     renderers = []
     for sensor in sensors:
         cd = cds.get(sensor)
-        renderers.append(plot_temperature.line(
-            x="local_datetime", y="temperature", source=cd, color=colors[sensor]
-        ))
-        renderers.append(plot_humidity.line(x="local_datetime", y="humidity", source=cd, color=colors[sensor]))
-        
+        renderers.append(
+            plot_temperature.line(
+                x="local_datetime", y="temperature", source=cd, color=colors[sensor]
+            )
+        )
+        renderers.append(
+            plot_humidity.line(
+                x="local_datetime", y="humidity", source=cd, color=colors[sensor]
+            )
+        )
+
     hover = HoverTool(
         tooltips=[
             ("Temperature", "@temperature{0.0} (°C)"),
@@ -127,9 +148,9 @@ def bokeh_plot(sensors):
         pn.pane.Bokeh(plot_humidity, sizing_mode="stretch_both"),
     )
 
+
 template = pn.template.BootstrapTemplate()
 template.sidebar.append(sensor_selection)
 template.main.append(bokeh_plot)
 
 template.servable()
-
